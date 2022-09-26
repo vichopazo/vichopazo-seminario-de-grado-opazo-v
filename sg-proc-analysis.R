692728d172b7ef46621a605aa1dca6bea31f3e65
vicente opazo cortés <vichopazo12@gmail.com>
2022-09-26 02:18:09 -0400

    Second commit
    url download for the corpus

--- sg-proc-analysis.R
+++ sg-proc-analysis.R
@@ -7,19 +7,21 @@ library(quanteda)
 library(readtext)
 library(stm)
 
-#Load readtext and ed_dataset, UNGDC_Chile_1970_2015
+#Load dataset and readtext, UNGDC_Chile_1970_2022
 
-raw_speeches <- readtext("C:/Users/vitoo/Onedrive/Documents/GitHub/seminario-de-grado-opazo-v/input/data/UNGDC_Chile_1970_2022/*.txt")
 
-                  
-ungd_chl <- readtext("C:/Users/vitoo/Onedrive/Documents/GitHub/seminario-de-grado-opazo-v/input/data/UNGDC_Chile_1970_2022/*.txt", 
-                      ignore_missing_files = FALSE, 
-                      text_field = NULL,
-                      docid_field = NULL, 
-                      docvarsfrom = "filenames", 
-                      dvsep = "_", 
-                      docvarnames = c("country", "session", "year"),  
-                      encoding = "UTF-8")
+url <- "https://www.researchgate.net/profile/Vicente_Opazo/publication/363844089_UN_General_Debate_Corpus_Chile_1970-2022/data/63313d086063772afd92b766/UNGDC-Chile-1970-2022.zip"
+
+download.file(url, "UNGC_Chile.zip")
+unzip("UNGC_Chile.zip")
+ungd_chl <- readtext("UNGDC_Chile_1970_2022", 
+                     ignore_missing_files = FALSE, 
+                     text_field = NULL,
+                     docid_field = NULL, 
+                     docvarsfrom = "filenames", 
+                     dvsep = "_", 
+                     docvarnames = c("country", "session", "year"),  
+                     encoding = "UTF-8")
 
 
 #Reshape corpus into paragraphs and create corpus from ed_dataset 
